255cbff2a994e831ecd8805ad85444afe006134a
Vanya <mayazurav@gmail.com>
2023-09-05 15:58:46 +0700

    tampilan awal sudah default, bug likes

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,21 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require("cors");
 var cookieParser = require("cookie-parser");
-
-// import error
-const errorHandler = require("./middleware/error");
+const path = require("path");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 // tambah config socket.io
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+
+// import error
+const errorHandler = require("./middleware/error");
 
 // import routes
 const authRoutes = require("./routes/authRoutes");
@@ -45,11 +51,50 @@ app.use(
 app.use(cookieParser());
 app.use(cors());
 
+// prevent SQL injection
+app.use(mongoSanitize());
+// adding security headers
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "https: data:"],
+    },
+  })
+);
+
+// prevent Cross-site Scripting XSS
+app.use(xss());
+//limit queries per 15mn
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+app.use(limiter);
+//HTTP Param Pollution
+app.use(hpp());
+
 // ROUTES MIDDLEWARE
 app.use("/api", authRoutes);
 app.use("/api", postRoute);
 app.use("/api", feebackRoute);
 app.use("/api", kategoriRoutes);
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 // error middleware
 app.use(errorHandler);
